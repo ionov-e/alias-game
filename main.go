7b566174cd82505653c76bb9d8915f9669228765
ionov-e <ioncurly@gmail.com>
2024-09-26 17:33:54 +0300

    Simple logger added

--- main.go
+++ main.go
@@ -6,14 +6,30 @@ import (
 	"go_telegram_start/telegram"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
+	mustSetUpLogging()
 	telegramClient := telegram.New(mustToken())
 	_, err := telegramClient.SendMessage("-4594910803", "Hello World!")
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func mustSetUpLogging() {
+	t := time.Now()
+	logDir := "log/" + t.Format("2006/01")
+	if err := os.MkdirAll(logDir, 0777); err != nil {
+		log.Fatal(err)
+	}
+	logPath := logDir + "/" + t.Format("02") + ".log"
+	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
 }
 
 // Ensures that token is provided via flag or .env
