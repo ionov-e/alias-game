734377a9050be0322c0cc23318d2b5f58020af2c
ionov-e <ioncurly@gmail.com>
2024-11-03 15:02:58 +0300

    Concurrency implemented for processing new updates

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,6 +6,8 @@ import (
 	"go_telegram_start/internal/database"
 	"go_telegram_start/internal/service/alias"
 	"go_telegram_start/pkg/telegram"
+	"log"
+	"sync"
 	"time"
 )
 
@@ -13,23 +15,32 @@ func Run(botToken string) error {
 	telegramClient := telegram.New(botToken)
 	storage := database.NewLocalRedis()
 	ctx := context.Background()
-	lastUpdateID := uint64(0)
+	offsetAsUpdateID := uint64(0)
 
 	for {
-		updates, err := telegramClient.GetUpdates(ctx, lastUpdateID, 10, 0)
+		updates, err := telegramClient.GetUpdates(ctx, offsetAsUpdateID, 10, 0)
 		if err != nil {
 			return fmt.Errorf("failed at getting telegram-updates: %w", err)
 		}
-		for _, update := range updates {
-			if update.UpdateID == lastUpdateID {
-				continue
-			}
-			lastUpdateID = update.UpdateID
-			game := alias.New(update, telegramClient, storage)
-			if err := game.Respond(ctx); err != nil {
-				return fmt.Errorf("failde at responding to update: %w", err)
-			}
+		if len(updates) > 0 {
+			offsetAsUpdateID = updates[len(updates)-1].UpdateID + 1 // Adds 1 to get the next update
 		}
+
+		var wg sync.WaitGroup
+		for _, update := range updates {
+			wg.Add(1)
+			go func(update telegram.Update) {
+				defer wg.Done()
+
+				game := alias.New(update, telegramClient, storage)
+
+				if err := game.Respond(ctx); err != nil {
+					log.Printf("Failed at responding to update: %+v, error: %v", update, err)
+				}
+			}(update)
+		}
+		wg.Wait()
+
 		time.Sleep(time.Second * 5)
 	}
 }
